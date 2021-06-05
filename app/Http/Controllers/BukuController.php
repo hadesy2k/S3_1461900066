cf77046539310ae5aec32df57bac9aad2acacce2
hadesy2k <ullam.risky@gmail.com>
2021-06-05 20:51:11 +0700

    S_1461900066

---
+++ app/Http/Controllers/BukuController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Buku;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class BukuController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $buku = DB::table('buku')->where("buku.judul", "LIKE", "%".$request->search."%")
+        ->orwhere("buku.tahun_terbit", "LIKE", "%".$request->search."%")
+            ->get();
+        return view('buku0066.index', [
+            'buku' => $buku
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('buku0066.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $buku = Buku::create([
+            'judul' => $request->judul,
+            'tahun_terbit' => $request->tahun,
+        ]);
+        return redirect('buku')->with('success', 'Data berhasil disimpan');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Buku  $buku
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Buku $buku)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Buku  $buku
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $buku = Buku::findorfail($id);
+        return view('buku0066.edit', compact('buku'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Buku  $buku
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $buku = [
+            'judul' => $request->judul,
+            'tahun_terbit' => $request->tahun,
+        ];
+
+        Buku::whereId($id)->update($buku);
+        return redirect('buku')->with('success', 'Data berhasil disimpan');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Buku  $buku
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $data = Buku::findorfail($id);
+        $data->delete();
+        return redirect('buku')->with('success', 'Data berhasil dihapus');
+    }
+}
